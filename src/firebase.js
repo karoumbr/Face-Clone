94dd688927e203f997e95bbe6da1ca8ecfb30e30
Karim Ben Romdhane <74336838+karoumbr@users.noreply.github.com>
2023-08-16 10:00:17 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBOWEDzxWCoPP_nh9wHxtE5dq1n2hdpxzI",
-    authDomain: "tp-facebookclone.firebaseapp.com",
-    projectId: "tp-facebookclone",
-    storageBucket: "tp-facebookclone.appspot.com",
-    messagingSenderId: "203390695316",
-    appId: "1:203390695316:web:b9e00954ac55c8ac654181",
-    measurementId: "G-XR1LL3ZJ52"
+    apiKey: "...",
+    authDomain: "...",
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "...",
+    appId: "...",
+    measurementId: "..."
   };
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 const db = firebaseApp.firestore()
